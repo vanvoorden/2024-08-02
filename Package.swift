c4c324d073b02db27f9e428955a2e55e50463bf2
Rick van Voorden <rick@northbronson.com>
2024-08-02 13:00:10 -0700

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -1,14 +1,26 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
-    name: "2024-08-02",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "2024-08-02"),
-    ]
+  name: "2024-08-02",
+  platforms: [
+    .macOS(.v14)
+  ],
+  targets: [
+    .executableTarget(
+      name: "2024-08-02",
+      resources: [
+        .process("../Resources/Info.plist")
+      ],
+      linkerSettings: [
+        .unsafeFlags([
+          "-Xlinker", "-sectcreate",
+          "-Xlinker", "__TEXT",
+          "-Xlinker", "__info_plist",
+          "-Xlinker", "./Resources/Info.plist",
+        ])
+      ]
+    ),
+  ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,93 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftData
 
-print("Hello, world!")
+//  https://forums.developer.apple.com/forums/thread/734540
+
+//  https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+
+@Model final public class Item {
+  var timestamp: Date
+  
+  public init(timestamp: Date = .now) {
+    self.timestamp = timestamp
+  }
+}
+
+func testDelete() throws {
+  let schema = Schema([Item.self])
+  let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try ModelContainer(
+    for: schema,
+    configurations: configuration
+  )
+  let modelContext = ModelContext(container)
+  modelContext.insert(Item())
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 1)
+  try modelContext.delete(model: Item.self)
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 0)
+}
+
+func testDeleteAndSave() throws {
+  let schema = Schema([Item.self])
+  let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try ModelContainer(
+    for: schema,
+    configurations: configuration
+  )
+  let modelContext = ModelContext(container)
+  modelContext.insert(Item())
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 1)
+  try modelContext.delete(model: Item.self)
+  try modelContext.save()
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 0)
+}
+
+func testDeleteWithIteration() throws {
+  let schema = Schema([Item.self])
+  let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try ModelContainer(
+    for: schema,
+    configurations: configuration
+  )
+  let modelContext = ModelContext(container)
+  modelContext.insert(Item())
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 1)
+  for model in try modelContext.fetch(FetchDescriptor<Item>()) {
+    modelContext.delete(model)
+  }
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 0)
+}
+
+func testDeleteWithIterationAndSave() throws {
+  let schema = Schema([Item.self])
+  let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try ModelContainer(
+    for: schema,
+    configurations: configuration
+  )
+  let modelContext = ModelContext(container)
+  modelContext.insert(Item())
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 1)
+  for model in try modelContext.fetch(FetchDescriptor<Item>()) {
+    modelContext.delete(model)
+  }
+  try modelContext.save()
+  print(try modelContext.fetchCount(FetchDescriptor<Item>()) == 0)
+}
+
+func main() throws {
+  try testDelete()
+  try testDeleteAndSave()
+  try testDeleteWithIteration()
+  try testDeleteWithIterationAndSave()
+}
+
+try main()
+//  true
+//  false
+//  true
+//  false
+//  true
+//  true
+//  true
+//  true
